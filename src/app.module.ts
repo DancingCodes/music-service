d23b844181ec6429f69ed25e26be9a531159c212
dancingcodes <dancingcodes@qq.com>
2025-07-24 16:18:25 +0800

    取消静态资源管理

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { MusicModule } from './modules/muisc/music.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { PUBLIC_DIR } from './config/global.constants';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: PUBLIC_DIR,
-      serveRoot: '/',
     }),
     DatabaseModule,
     MusicModule
